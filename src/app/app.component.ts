604ce7f526ca48b3257508ef2e7dc6a8f0759c05
Rustam <latyminati@gmail.com>
2023-05-05 09:08:46 +0530

    simple view form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pract1';
+fullname: string="";
+email:string="";
+password:string="";
+confirmpassword:string="";
+gender:string="";
+course:string="";
+country:string="";
+comment:string="";
+licenseAgreement:boolean=false;
+msg:string="";
+msg2:Array<string>=[];
+  RegisterStudent(studentForm: NgForm): void {   
+    console.log(studentForm.value);
+       this.msg = "Name : "+this.fullname
+       +"<br/>Email:"+this.email
+        +"<br/>Password:"+this.password
+       +"<br/>ConfirmPassword:"+this.confirmpassword
+       +"<br/>Gender : "+this.gender
+       +"<br/>Country : "+this.country
+        +"<br/>comment:"+this.comment
+       +"<br/>License Agreement : "+this.licenseAgreement;
+      this.msg2= this.msg.split('<br/>');
+  }
+
 }
